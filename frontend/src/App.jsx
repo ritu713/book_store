1c2488a4aa3e19169a62d54fee5141f2cbf85a5d
Ritu <singh.ritu.rajiv@gmail.com>
2023-12-17 14:15:25 +0530

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+import Home from "./pages/Home.jsx"
+import CreateBook from './pages/createBook.jsx'
+import ShowBook from './pages/showBook.jsx'
+import EditBook from './pages/editBook.jsx'
+import DeleteBook from './pages/deleteBook.jsx'
 
 const App = () => {
+  console.log("Entered app")
   return (
-    <div className='bg-red-400 text-white'>
-      App...
-    </div>
+    <Routes>
+      <Route path='/' element={ <Home/> }></Route>
+      <Route path='/books/create' element={ <CreateBook/> }></Route>
+      <Route path='/books/details/:id' element={ <ShowBook/> }></Route>
+      <Route path='/books/edit/:id' element={ <EditBook/> } ></Route>
+      <Route path='/books/delete/:id' element={ <DeleteBook/> }></Route>
+    </Routes>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   // Specify the entry point for the build
+  //   rollupOptions: {
+  //     input: '.src/main.jsx',
+  //   }
+  // }
 })
